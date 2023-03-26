0b1ea33f31ae613cdeaddc6e41e0673ef958375f
katerineBarahona <116216637+katerineBarahona@users.noreply.github.com>
2023-03-25 19:28:10 -0600

    vabien
    va bien

---
+++ Examen Segundo Parcial/BdDatos/UsuarioDB.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdDatos
+{
+    public  class UsuarioDB
+    {
+        string cadena = "server=localhost; User=root; database=factura; password=123456";
+        
+        public Usuario 
+    }
--- Examen Segundo Parcial/Vistas/Program.cs
+++ Examen Segundo Parcial/Vistas/Program.cs
@@ -16,7 +16,7 @@ namespace Vistas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Loginform());
         }
     }
 }
